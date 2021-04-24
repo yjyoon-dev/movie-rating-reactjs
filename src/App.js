a5aa5cf02d7e167e7ddf6982ff110fe99e952e40
unknown <yjyoon.dev@gmail.com>
2021-04-25 02:08:14 +0900

    prop-types 를 활용한 인자 형식 확인

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+function Print({name, image, rating}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>{name}</h3>
+      <img src={image} />
+      <h4>{rating}</h4>
     </div>
   );
 }
+
+Print.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const favList = [
+  {
+    id:1,
+    name:"fromis9",
+    image:"https://storage3.ilyo.co.kr/contents/article/images/2020/0917/1600316908454180.jpg",
+    rating:4.9
+  },
+  {
+    id:2,
+    name:"bravegirls",
+    image:"https://img.sportsworldi.com/content/image/2021/03/20/20210320505771.jpg",
+    rating:5.0
+  },
+  {
+    id:3,
+    name:"rollin",
+    image:"https://image.ytn.co.kr/general/jpg/2021/0304/202103041352125044_d.jpg",
+    rating:5.0
+  }
+];
+
+function rendorFav(fav) {
+  return <Print name={fav.name} image={fav.image} rating={fav.rating}/>;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      {favList.map(rendorFav)}
+    </div>
+  );
+}
+
 
 export default App;
